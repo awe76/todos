1982dc3c8debbd38522e254cff48409b0e287723
Andrei Silchankau <andrei.silchankau@hqo.co>
2022-11-18 19:39:37 +0100

    todos initial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import todos from '../stores/todo';
+    import type {Todo} from '../stores/todo';
+
+    let todoTitle = '';
+    let todoInput: HTMLInputElement;
+
+    const addTodo = () => {
+        const todo: Todo = {
+            title: todoTitle,
+            done: false,
+        };
+
+        todos.update((current) => [...current, todo]);
+        todoInput.focus();
+        todoTitle = '';
+    }
+
+
+</script>
+
+<h1>Todo List</h1>
+
+<main>
+    <label>
+        Add Todo:
+        <input bind:this={todoInput} bind:value={todoTitle} on:change={addTodo} />
+    </label>
+    <button on:click={addTodo}>Add</button>
+
+    <div>
+        <h2>To do:</h2>
+        <ul>
+            {#each $todos as {title}}
+                <li>{title}</li>
+            {/each}
+        </ul>
+    </div>
+</main>
---
+++ src/stores/todo.ts
@@ -0,0 +1,10 @@
+import { writable } from 'svelte/store';
+
+export type Todo = {
+    title: string
+    done: boolean
+}
+
+const todos = writable<Todo[]>([]);
+export default todos;
+
